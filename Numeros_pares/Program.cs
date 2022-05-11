e435a59486764043bb1e58dd7963029457d36302
Nivaldo almeida <102814392+nivaldoalmeida@users.noreply.github.com>
2022-05-10 22:27:17 -0300

    Add files via upload

---
+++ Numeros_pares/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Intervalo_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+	int vetor[5];
+	int i, pares, impares, negativos, positivos;
+	i = pares = impares = negativos = positivos = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &vetor[i]);
+		if (vetor[i] % 2 != 0)
+		{
+			impares++;
+		}
+		else if (vetor[i] % 2 == 0)
+		{
+			pares++;
+		}
+		if (vetor[i] > 0)
+		{
+			positivos++;
+		}
+		else if (vetor[i] < 0)
+		{
+			negativos++;
+		}
+	}
+			printf("%d valor(es) par(es)\n%d valor(es) impar(es)\n%d valor(es) positivo(s)\n%d valor(es) negativo(s)\n", pares, impares, positivos, negativos);
+	return 0;
+}
