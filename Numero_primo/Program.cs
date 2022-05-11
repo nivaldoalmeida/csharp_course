faf57680f215909845e084a12f1c66a6f9c13399
Nivaldo almeida <102814392+nivaldoalmeida@users.noreply.github.com>
2022-05-10 21:11:47 -0300

    Add files via upload

---
+++ Numero_primo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numero_primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int X = int.Parse(Console.ReadLine());
+                int count = 0;
+                for (int j = 1; j <= X; j++)
+                {
+                    if (X % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(X + " eh primo");
+                }
+                else
+                {
+                    Console.WriteLine(X + " nao eh primo");
+                }
+            }
+        }
+    }
+    //#By nivaldo
+}
