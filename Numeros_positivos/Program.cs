e7ff1e2fd62cf77384bc6e0122c3e3963a3c1fa2
Nivaldo almeida <102814392+nivaldoalmeida@users.noreply.github.com>
2022-05-10 22:26:59 -0300

    Add files via upload

---
+++ Numeros_positivos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Numeros_positivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1 = double.Parse(Console.ReadLine());
+            double valor2 = double.Parse(Console.ReadLine());
+            double valor3 = double.Parse(Console.ReadLine());
+            double valor4 = double.Parse(Console.ReadLine());
+            double valor5 = double.Parse(Console.ReadLine());
+            double valor6 = double.Parse(Console.ReadLine());
+            int ehPositivo = 0;
+
+            if (valor1 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            if (valor2 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            if (valor3 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            if (valor4 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            if (valor5 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            if (valor6 > 0.0)
+            {
+                ehPositivo += 1;
+            }
+
+            Console.WriteLine(ehPositivo + " valores positivos");
+        }
+    }
+    //#By nivaldo
+}
