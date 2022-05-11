8245e59b8f80ac4fb7a81201d966cf3f2f6de824
Nivaldo almeida <102814392+nivaldoalmeida@users.noreply.github.com>
2022-05-10 21:12:28 -0300

    Add files via upload

---
+++ Números_Ímpares/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numeros_impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
