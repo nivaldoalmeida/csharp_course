094223f9d788cfabc2a44266ede9215fdd7c579b
Nivaldo almeida <102814392+nivaldoalmeida@users.noreply.github.com>
2022-05-10 22:25:56 -0300

    Add files via upload

---
+++ Pum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int primeiro = 5;
+            for (int i = 5; i <= N; i++)
+            {
+
+                int segundo = primeiro + 5;
+                int terceiro = primeiro + 10;
+                Console.WriteLine(primeiro + " " + segundo + " " + terceiro + " sol");
+
+               
+            }
+        }
+    }
+    //#By nivaldo
+}
